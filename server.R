2e41f509f778006026643073513225337a56dd26
j-b-ferguson <65960398+j-b-ferguson@users.noreply.github.com>
2021-02-05 03:00:07 +1000

    v1

---
+++ server.R
@@ -0,0 +1,108 @@
+library(shiny) # Shiny web apps
+library(shinydashboard)
+library(mlr) # For machine learning in R
+library(forecast) # For Boxcox and inverse Boxcox transformations
+library(readr) # Read csv files
+library(magrittr) # For pipe symbols
+
+
+
+
+# Define server logic for prediction model
+server <- function(input, output) {
+    
+    # Read required data sets from file
+    df.vic <- read_csv(
+      normalizePath(file.path('data/VICTORIA_COVID19.csv')))
+    df.vicx.optimal <- read_csv(
+      normalizePath(file.path('data/VICTORIA_COVID19x.csv')))
+    
+    # Create a regression task
+    cases.taskx <- makeRegrTask(data = df.vicx.optimal, target = 'NextDayCasesx')
+    
+    # Create a linear regression learner
+    lrn <- makeLearner(cl = 'regr.glm')
+
+    # Subset model training set
+    set.seed(1234)
+    n <- nrow(df.vicx.optimal)
+    train.set <- sample(n, size = 4/5*n)
+    
+    # Fit the model
+    model.optimal <- train(lrn, cases.taskx, train.set)
+    
+    # Define reactive events --------------------------------------------------
+    
+    # Take input from user for 7 Day COVID-19 Case Moving Average and transform into the Boxcox equivalent value
+    SvnDayCaseAvgx <- eventReactive(input$Run_model, {
+      BoxCox(input$input1, lambda = BoxCox.lambda(df.vic$SvnDayCaseAvg, method = 'guerrero'))
+    }, ignoreNULL = FALSE)
+    
+    # Take input from user for 14 Day COVID-19 Case Moving Average and transform into the Boxcox equivalent value
+    FrtnDayCaseAvgx <- eventReactive(input$Run_model, {
+      BoxCox(input$input2, lambda = BoxCox.lambda(df.vic$FrtnDayCaseAvg, method = 'guerrero'))
+    }, ignoreNULL = FALSE)
+    
+    # Take input from user for 7 Day COVID-19 Test Moving Average and transform into the Boxcox equivalent value
+    SvnDayTestAvgx <- eventReactive(input$Run_model, {
+      BoxCox(input$input3, lambda = BoxCox.lambda(df.vic$SvnDayTestAvg , method = 'guerrero'))
+    }, ignoreNULL = FALSE)
+    
+    # Create a new data frame from the user inputs to run linear model predictions
+    newdata <- eventReactive(input$Run_model, {
+      data.frame(SvnDayCaseAvgx = SvnDayCaseAvgx(), 
+                 FrtnDayCaseAvgx = FrtnDayCaseAvgx(), 
+                 SvnDayTestAvgx = SvnDayTestAvgx())
+    }, ignoreNULL = FALSE)
+    
+    # Run linear model predictions and convert the results with an inverse Boxcox transformation
+    pred <- eventReactive(input$Run_model, {
+      boxcox.bounds <- predict.lm(
+                          lm(NextDayCasesx ~ SvnDayCaseAvgx + FrtnDayCaseAvgx + SvnDayTestAvgx, data = df.vicx.optimal), 
+                          newdata = newdata(), 
+                          interval = "predict") %>% 
+                          as.data.frame()
+      
+      df.actual.bounds <- data.frame(
+        InvBoxCox(boxcox.bounds$lwr, lambda = BoxCox.lambda(df.vic$NextDayCases, method = 'guerrero')) %>% round(0),
+        InvBoxCox(boxcox.bounds$fit, lambda = BoxCox.lambda(df.vic$NextDayCases, method = 'guerrero')) %>% round(0),
+        InvBoxCox(boxcox.bounds$upr, lambda = BoxCox.lambda(df.vic$NextDayCases, method = 'guerrero')) %>% round(0)
+      )
+      df.actual.bounds
+    }, ignoreNULL = FALSE)
+    
+    # Emoji visualisation logic
+    img.logic <- eventReactive(input$Run_model, {
+      if (pred()[[2]] > input$input4) {
+        increase <- data.frame('chart-increasing-emoji-clipart-md.png', 
+                      paste0('The model predicts an increase in COVID-19 cases compared with today. The lower and upper bounds of the 95% prediction interval are ', pred()[[1]], ' and ', pred()[[3]], '.'))
+        
+        increase
+      } else if (input$input4 > pred()[[2]]) {
+        decrease <- data.frame('chart-decreasing-emoji-clipart-md.png', 
+                      paste0('The model predicts a decrease in COVID-19 cases compared with today. The lower and upper bounds of the 95% prediction interval are ', pred()[[1]], ' and ', pred()[[3]], '.'))
+        
+        decrease
+      } else if (pred()[[2]] == input$input4) {
+        nochange <- data.frame('nochange.jpeg',
+                               paste0('The model predicts no change in COVID-19 cases compared with today. The lower and upper bounds of the 95% prediction interval are ', pred()[[1]], ' and ', pred()[[3]], '.'))
+        
+        nochange
+      }
+    }, ignoreNULL = FALSE)
+    
+    
+    
+    
+    
+    # Show text of fitted prediction
+    output$text1 <- renderText(pred()[[2]])
+    output$text2 <- renderText(img.logic()[[2]])
+    output$image1 <- renderImage({
+      filename <- normalizePath(file.path('./images',
+                                paste(img.logic()[1])))
+  
+      # Return a list containing the filename
+      list(src = filename)
+    }, deleteFile = FALSE)
+}
---
+++ ui.R
@@ -0,0 +1,183 @@
+library(shiny) # Shiny web apps
+library(shinydashboard)
+library(mlr) # For machine learning in R
+library(forecast) # For Boxcox and inverse Boxcox transformations
+library(readr) # Read csv files
+library(magrittr) # For pipe symbols
+
+
+
+# Define UI for prediction model
+ui <- shinyUI(dashboardPage(
+  
+  dashboardHeader(title = "Case Predictor", titleWidth = "230px"),
+  
+  dashboardSidebar(
+      
+    sidebarMenu(
+      
+      # Sidebar menu header
+      menuItem("Predict COVID-19 Cases!", tabName = "model", icon = icon("bar-chart-o"), startExpanded = TRUE,
+        
+        # Sidebar menu inputs       
+        numericInput(inputId = "input1", 
+                     label = "7 Day COVID-19 Case Moving Average", 
+                     value = 20, 
+                     min = 0, 
+                     max = 500),
+        
+        numericInput(inputId = "input2", 
+                     label = "14 Day COVID-19 Case Moving Average", 
+                     value = 20, 
+                     min = 0, 
+                     max = 500),
+        
+        numericInput(inputId = "input3", 
+                     label = "7 Day COVID-19 Test Moving Average", 
+                     value = 20, 
+                     min = 0, 
+                     max = 10000),
+        
+        numericInput(inputId = "input4", 
+                     label = "Cases Today", 
+                     value = 20, 
+                     min = 0, 
+                     max = 500),
+        
+        # Predict action button
+        div(style="display: inline-block; vertical-align:top; width: 100px;",
+          actionButton("Run_model", "Predict!")),
+        
+        # Author details and social links
+        div(style="display: block; vertical-align:top; width:0px; margin-left: -10px;",
+          fluidRow(
+            column(width = 1,
+              box('A Shiny web application by Justin', br(), 'Ferguson', br(), br(),
+                  tags$a(href="https://www.justinferguson.me/", icon("globe", "fa-3x")),
+                  tags$a(href="https://www.linkedin.com/in/j-b-ferguson/", icon("linkedin", "fa-3x")),
+                  tags$a(href="https://github.com/j-b-ferguson/", icon("github", "fa-3x"))))))
+        ))),
+  
+  dashboardBody(
+    
+        # Custom styles CSS3 and HTML5
+        tags$head(tags$style(
+          "@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300&display=swap');
+          * {font-family: 'Source Sans Pro', 'sans-serif';
+             font-weight: 300;}
+          h1 {font-family: 'Source Sans Pro', 'sans-serif';
+              font-weight: 200;}"
+        )),
+        
+        tags$head(tags$style(
+            type="text/css",
+            "#image1 img {height: auto; 
+                          width: 200px; 
+                          margin-left: auto; 
+                          margin-right: auto;}"
+        )),
+        
+        tags$head(tags$style("@import url('https://fonts.googleapis.com/css2?family=Caveat&display=swap');
+                              .box-body {color: black;
+                                         font-size: 16px;
+                                         font-weight: 600;
+                                         font-family: 'Caveat', 'sans-serif';
+                                         color: white;}"
+        )),
+        
+        tags$head(tags$style(
+            type="text/css",
+            ".fa-globe {padding-right: 30px; 
+                        padding-top: -50px; 
+                        font-size: 40px;}
+             .fab {padding-right: 30px; 
+                   font-size: 40px;}"
+        )),
+        
+        tags$head(tags$style("#text1{color: black;
+                                     font-size: 32px;
+                                     font-family: Source Sans Pro;}"
+        )),
+        
+        tags$head(tags$style(
+          type="text/css",
+            ".box-header h3 {font-weight: bold;}"
+        )),
+    
+        tags$style(HTML(
+          ".info-box {background:#00000000;}"
+        )),
+    
+    # Set main border
+    div(style="box-shadow: 2px 2px 5px grey; 
+               margin-top: 29px;",
+        
+        # Set prediction output
+        div(style="display: flex; 
+                   margin-left: 85px; 
+                   margin-bottom: 20px; 
+                   padding-top: 43px;",
+            
+          h2("The predicted COVID-19 cases tomorrow is:",HTML('&nbsp;')),
+          h2(textOutput("text1"))
+        ),
+        
+        # Image and floating text
+        fluidRow(
+          
+          div(style="display: flex; 
+                     margin-left: 100px; 
+                     margin-bottom: 73px; 
+                     float: left;",
+              
+            imageOutput(outputId = "image1"),
+          ),
+          
+          column(width = 3,
+            
+            div(style="display: flex; 
+                       position: fixed; 
+                       margin-left: 20px; 
+                       margin-right: 100px; 
+                       margin-top: -10px; 
+                       padding-top: 0px; 
+                       float: left;",
+                
+              h3(textOutput("text2"))
+            ))),
+      
+      # Inline info boxes    
+      div(style = "display: inline-block; 
+                   position: fixed; 
+                   margin-left: 80px; 
+                   margin-top: -200px;",   
+          
+          infoBox(
+            "What", "is this model for?", icon = icon("line-chart"),
+            subtitle = 'This model has been fitted from Victorian (Australia) COVID-19 data in order to predict tomorrow\'s cases.',
+            width = 3
+          ),
+          
+          infoBox(
+            "How", "do I use this model?", icon = icon("question"),
+            subtitle = 'Use 7 and 14 day COVID-19 case and test averages to make a prediction. The prediction is then compared with today\'s cases.',
+            width = 3
+          ),
+          
+          infoBox(
+            "Where", "can I find out more?", icon = icon("book-open"),
+            subtitle = 'See how the model was fitted and tested at this link. For active COVID-19 data, please visit covid19data.com.au or the WHO\'s website.',
+            width = 3,
+            href = 'https://www.covid19data.com.au/'
+          ),
+          
+          infoBox(
+            "Who", "is this model for?", icon = icon("user-friends"),
+            subtitle = 'Everyone! Please use this app and share with other people. If you have any questions or suggestions, please get in touch.',
+            width = 3,
+            href = 'mailto:justin.benjamin.ferguson@gmail.com?subject=COVID-19%20Model%20Enquiry'
+          ))
+      )
+    )
+  )
+)
